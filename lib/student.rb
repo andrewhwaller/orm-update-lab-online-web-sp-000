dec70c75c893c5008a8ad016446a1c8d9ad8fdde
Andrew Waller <andrewhwaller@gmail.com>
2019-04-15 01:02:34 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -58,4 +58,12 @@ class Student
     grade = row[2]
     self.new(id, name, grade)
   end
+
+  def self.find_by_name(name)
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE name = ?
+      LIMIT 1
+    SQL
 end
